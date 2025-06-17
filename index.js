e976c0e8357dcb8b6357ad59c0468013affa1393
Fionax8 <fiona.kalinowska@gmail.com>
2025-06-17 19:31:47 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/", (req, res) => {
+  res.send("Supermarkt-API läuft!");
+});
+
+app.get("/edeka_angebote.json", (req, res) => {
+  fs.readFile("edeka_angebote.json", "utf8", (err, data) => {
+    if (err) return res.status(500).send("Fehler beim Lesen der Datei");
+    res.setHeader("Content-Type", "application/json");
+    res.send(data);
+  });
+});
+
+app.get("/kaufland_angebote.json", (req, res) => {
+  fs.readFile("kaufland_angebote.json", "utf8", (err, data) => {
+    if (err) return res.status(500).send("Fehler beim Lesen der Datei");
+    res.setHeader("Content-Type", "application/json");
+    res.send(data);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
